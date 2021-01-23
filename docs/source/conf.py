a8c95724c2a7663ef7143e1257fa359b6eb5623f
Jirka Borovec <jirka.borovec@seznam.cz>
2021-01-23 23:28:07 +0100

    paths

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -27,7 +27,6 @@ sys.path.insert(0, os.path.abspath(PATH_ROOT))
 
 FOLDER_GENERATED = 'generated'
 SPHINX_MOCK_REQUIREMENTS = int(os.environ.get('SPHINX_MOCK_REQUIREMENTS', True))
-# fixme
 builtins.__LIGHTNING_SETUP__ = True
 
 import pytorch_lightning  # noqa: E402
